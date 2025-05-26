b34c05f9ad57fd63818e3eb5fe1c251c2411e9b7
krcdb <memartin@student.42.fr>
2025-05-26 16:08:06 +0200

    chore: comments

--- setup_venv.sh
+++ setup_venv.sh
@@ -1,21 +1,16 @@
 #!/bin/bash
 
-# Name of the virtual environment folder
 VENV_NAME=".venv"
 
-# Create virtual environment
 python3 -m venv "$VENV_NAME"
 
-# Check if creation was successful
 if [ $? -ne 0 ]; then
   echo "❌ Failed to create virtual environment."
   exit 1
 fi
 
-# Activate virtual environment
 source "$VENV_NAME/bin/activate"
 
-# Upgrade pip inside the virtual environment
 pip install --upgrade pip
 
 echo "✅ Virtual environment '$VENV_NAME' created and activated."
